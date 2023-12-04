e8e4ef503a07930b5aabdda499bbb40e4de1e7cc
admin <admin@DESKTOP-5TLN6O9>
2023-12-04 12:00:14 +0800

    Добавьте файлы проекта.

---
+++ LinkedList/Item.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    // <T> это тип хранимых данных
+    internal class Item<T>
+    {
+        public T Data { get; set; } // Хранимые данные
+        public Item<T> Next { get; set; } // Следующий элемент связанного списка
+        public Item(T data) // Создание нового экземпляра связаного списка
+        {
+            if (data == null) // Проверка данных, если пустые тогда выдаем ошибку
+            {
+                throw new ArgumentNullException(nameof(data));
+            }
+            Data = data; // Если не пустые, то все хорошо
+        }
+        public override string ToString() //Приведение объекта к строке.
+        {
+            return Data.ToString();
+        }
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new LinkedList<int>();
+            list.Add(43);
+            list.Add(2);
+            list.Add(31231);
+            list.Add(-12);
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+            list.Remove(31231);
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
